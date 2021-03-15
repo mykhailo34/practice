db31bd1de2fca8d2fae419ba290f6aece61d6113
mykhailo34 <trifonenkomisha@gmail.com>
2021-03-15 17:51:28 +0200

    tree homework assignment

---
+++ JS/script.js
@@ -0,0 +1,158 @@
+'use strict';
+//  Задание на урок:
+// 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+// 'Сколько фильмов вы уже посмотрели?'
+// 2) Создать объект personalMovieDB и в него поместить такие свойства:
+//     - count - сюда передается ответ на первый вопрос
+//     - movies - в это свойство поместить пустой объект
+//     - actors - тоже поместить пустой объект
+//     - genres - сюда поместить пустой массив
+//     - privat - в это свойство поместить boolean(логическое) значение false
+// 3) Задайте пользователю по два раза вопросы:
+//     - 'Один из последних просмотренных фильмов?'
+//     - 'На сколько оцените его?'
+// Ответы стоит поместить в отдельные переменные
+// Записать ответы в объект movies в формате: 
+//     movies: {
+//         'logan': '8.1'
+//     }
+
+// const namberOfFilms = +prompt ('Скільки фільмів ви подивились?','');
+// const personalMovieDB ={
+//     count:namberOfFilms,
+//     movies: {},
+//     actors:{},
+//     genres:[],
+//     privat: false
+// };
+// const a = prompt ('Скільки фільмів ви подивились?',''),
+//       b = prompt ('На скільки оцірите цей фільм?',''),
+//       c = prompt ('Скільки фільмів ви подивились?',''),
+//       d = prompt ('На скільки оцірите цей фільм?','');
+
+//  personalMovieDB.movies[a] = b;
+//  personalMovieDB.movies[c] = d;
+ 
+//  console.log(personalMovieDB);
+
+// Цикли
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// const namberOfFilms = +prompt ('Скільки фільмів ви подивились?','');
+// const personalMovieDB ={
+//     count:namberOfFilms,
+//     movies: {},
+//     actors:{},
+//     genres:[],
+//     privat: false
+// };
+
+//  for (let i = 0; i < 2; i++){
+//     const a = prompt ('Скільки фільмів ви подивились?',''),
+//         b = prompt ('На скільки оцірите цей фільм?','');
+
+//         if (a != null && b != null && a != '' && b != '' && a.length <50) {
+//             personalMovieDB.movies[a] = b;
+//              console.log('Done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+       
+//  }
+//  if (personalMovieDB.count < 10) {
+//      console.log("Переглянуто дуже мало фільмів");
+//  } else if (personalMovieDB >= 10 && personalMovieDB.count < 30) {
+//       console.log("Ви класичний глядач");
+//  } else if (personalMovieDB.count >= 30) {
+//      console.log("Ви кіноман");
+//  } else {
+//      console.log("Помилка");
+//  }
+//  console.log(personalMovieDB);
+ 
+//2 практика використовуємо функції
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+let namberOfFilms;
+function start() {
+    namberOfFilms = +prompt ('Скільки фільмів ви подивились?','');
+    while (namberOfFilms == '' || namberOfFilms == null || isNaN (namberOfFilms)) {
+          namberOfFilms = +prompt ('Скільки фільмів ви подивились?','');
+
+    }
+}
+start();
+const personalMovieDB ={
+    count:namberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat: false
+};
+ function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        const a = prompt ('Скільки фільмів ви подивились?',''),
+            b = prompt ('На скільки оцірите цей фільм?','');
+    
+            if (a != null && b != null && a != '' && b != '' && a.length <50) {
+                personalMovieDB.movies[a] = b;
+                 console.log('Done');
+            } else {
+                console.log('error');
+                i--;          
+     } 
+ }
+}
+ rememberMyFilms();
+  function detectPersonalLevel(){
+    rememberMyFilms();
+    if (personalMovieDB.count < 10) {
+        console.log("Переглянуто дуже мало фільмів");
+    } else if (personalMovieDB >= 10 && personalMovieDB.count < 30) {
+         console.log("Ви класичний глядач");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Ви кіноман");
+    } else {
+        console.log("Помилка");
+    }
+  }
+  detectPersonalLevel();
+   
+  function showMyDB (hidden){
+     if (!hidden)
+     console.log(personalMovieDB);
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимий жанр під номером ${i}`);
+
+    }
+}
+writeYourGenres()
+
+  
